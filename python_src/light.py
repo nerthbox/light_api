5ac92f548f5ba27ad8c6aa550982c7ab6502bf70
nerthbox <njennings@softnas.com>
2017-02-26 18:16:50 -0500

    fixed __init__ logic order for setmode

--- python_src/light.py
+++ python_src/light.py
@@ -6,6 +6,8 @@ class Light(object):
         self.channel = channel
         self.direction = direction
         self.input_value = None
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
         if(self.direction == "output"):
             self._init_output()
         elif(self.direction == "input"):
@@ -13,9 +15,6 @@ class Light(object):
         else:
             print "direction must be 'input' or 'output'"
             exit(1)
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setwarnings(False)
 
     def _init_output(self):
         GPIO.setup(self.channel, GPIO.OUT, initial=GPIO.LOW)
